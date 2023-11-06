ab384f8d15d87c588b7a7250d0b5521c9fb2a7bb
Destroier007 <nickolas.ng@hotmail.com>
2023-11-06 15:37:55 -0300

    att 0.2

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,9 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import{Server}from "socket.io";
+
 
 const app = express();
 const porta = process.env.porta || 3000;
@@ -9,4 +12,10 @@ const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
 
-app.listen(porta, () => console.log(`Server escutando na porta ${porta}`));
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(porta, () => console.log(`Server escutando na porta ${porta}`));
+
+const io = new Server(servidorHttp);
+
+export default io;
